8da1d76706acde2503c3f4fb2f19d206d530320c
Kyle Ovington <gamer@kek.ie>
2019-09-27 14:25:26 +0100

    actually removed previous from Node class this time

--- src/Node.java
+++ src/Node.java
@@ -1,11 +1,10 @@
 public class Node<T> {
     T data;
-    Node<T> left, right, previous;
+    Node<T> left, right;
 
     public Node(T nodeData) {
         data = nodeData;
         left = null;
         right = null;
-        previous = null;
     }
 }
