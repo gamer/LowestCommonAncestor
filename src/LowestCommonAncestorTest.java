9550c48aef9508020156d19bdd52e76cd51445cd
Kyle Ovington <gamer@kek.ie>
2019-10-15 18:48:09 +0100

    added tests for LCA on DAG

--- src/LowestCommonAncestorTest.java
+++ src/LowestCommonAncestorTest.java
@@ -8,16 +8,49 @@ public class LowestCommonAncestorTest {
     @Test
     public void findLCATest() {
         Node<Integer> root = new Node<>(7);
-        root.right = new Node<>(8);
-        root.right.right = new Node<>(1);
-        root.right.left = new Node<>(9);
-        root.left = new Node<>(5);
-        root.left.left = new Node<>(4);
-        root.left.left.left = new Node<>(2);
-        root.left.right = new Node<>(3);
-        Assertions.assertEquals(root.right.data, LowestCommonAncestor.findLCA(root, root.right.right, root.right.left).data,
+        root.addRight(new Node<>(8));
+        root.getRight().addRight(new Node<>(1));
+        root.getRight().addLeft(new Node<>(9));
+        root.addLeft(new Node<>(5));
+        root.getLeft().addLeft(new Node<>(4));
+        root.getLeft().getLeft().addLeft(new Node<>(2));
+        root.getLeft().addRight(new Node<>(3));
+        Assertions.assertEquals(root.getRight().data, LowestCommonAncestor.findLCA(root, root.getRight().getRight(), root.getRight().getLeft()).data,
                 "simple 2 level sub tree expecting 8, LCA(root, 1, 9)");
-        Assertions.assertEquals(root.data, LowestCommonAncestor.findLCA(root, root.right.right, root.left.left.left).data,
+        Assertions.assertEquals(root.data, LowestCommonAncestor.findLCA(root, root.getRight().getRight(), root.getLeft().getLeft().getLeft()).data,
                 "furthest extent opposite sides expecting root, LCA(root, 1, 2)");
+    }
+
+    @Test
+    public void findLCADAGTest() {
+        Node<String> G = new Node<>("G");
+        Node<String> F = new Node<>("F");
+        Node<String> E = new Node<>("E");
+        Node<String> D = new Node<>("D");
+        Node<String> C = new Node<>("C");
+        Node<String> B = new Node<>("B");
+        Node<String> A = new Node<>("A");
+        G.depth = 0;
+        F.depth = D.depth = 1;
+        E.depth = C.depth = 2;
+        B.depth = 3;
+        A.depth = 4;
+
+        G.children.add(F); G.children.add(D);
+        F.parents.add(G); D.parents.add(G);
+        F.children.add(E);
+        E.parents.add(F);
+        D.children.add(C);
+        C.parents.add(D);
+        C.children.add(B);
+        E.children.add(B);
+        B.parents.add(C); B.parents.add(E);
+        B.children.add(A);
+        A.parents.add(B);
+
+        Assertions.assertEquals("G", LowestCommonAncestor.findLCADAG(G,F,C).data,"LCADAG(G F C)");
+        Assertions.assertEquals("G", LowestCommonAncestor.findLCADAG(G,F,C).data,"LCADAG(G E B)");
+        Assertions.assertEquals("G", LowestCommonAncestor.findLCADAG(G,F,C).data,"LCADAG(G F D)");
+        Assertions.assertEquals(null, LowestCommonAncestor.findLCADAG(null,null,null), "null input");
     }
 }
